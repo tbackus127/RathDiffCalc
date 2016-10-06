32f97127699c188a8b1c7acc96626d72df4fcb61
Tim Backus <tbackus127@gmail.com>
2016-10-06 13:39:36 -0400

    Begin map parser.
    Signed-off-by: Tim Backus <tbackus127@gmail.com>

--- src/com/rath/beatmap/MapParser.java
+++ src/com/rath/beatmap/MapParser.java
@@ -1,13 +1,44 @@
+
 package com.rath.beatmap;
 
 import java.io.File;
-
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class MapParser {
 
-  
-  public Beatmap parse(File osuFile) {
-    //TODO: Return beatmap object
+  private static final String[] labelList = { "StackLeniency", "Title",
+      "Artist", "Creator", "Version", "Source", "HPDrainRate", "CircleSize",
+      "OverallDifficulty", "ApproachRate", "SliderMultiplier", "SliderTickRate" };
+
+  public Beatmap parse(File osuFile) throws FileNotFoundException {
+
+    if (osuFile.exists()) {
+      Scanner fileScan = null;
+      try {
+        fileScan = new Scanner(osuFile, "UTF-8");
+        return parse(fileScan);
+      }
+      catch (IOException ioe) {
+        ioe.printStackTrace();
+      }
+      finally {
+        fileScan.close();
+      }
+    } else {
+      throw new FileNotFoundException("Beatmap file does not exist!");
+    }
+    return null;
+  }
+
+  private Beatmap parse(Scanner fscan) {
+    for (int i = 0; i < labelList.length && fscan.hasNextLine(); i++, fscan
+        .nextLine()) {
+      String line = fscan.nextLine();
+      
+    }
+
     return null;
   }
 }
